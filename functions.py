93e5d5a42c6c142b8082271d556112ea2d732e39
Alfredo Perez <alfredoperez1998@gmail.com>
2017-11-25 00:08:50 -0600

    Variables from the configuration file implemented and some inecesary lines were deleted.

--- functions.py
+++ functions.py
@@ -6,53 +6,57 @@ import matplotlib.patches as mpatches
 import csv
 from numpy import arange
 from pickle import load
+from configuration import *
 
 def draw_graph(x, y, graph, eq, v_x, v_y, u_x, u_y):
-    latex = input("Do you want to use LaTeX for text rendering? (y/n) ")
-    if latex == 'y':
-        rc('text', usetex=True)
-        rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
-        rcParams['axes.labelsize'] = 15
-        if graph == 1:
-            plt.plot(x, y, color = '#361156', ls = '-', marker = '.')
-            plt.xlabel('$' + v_x + "(" + u_x + ")" + '$')
-            plt.ylabel('$' + v_y + "(" + u_y + ")" + '$')
-            plt.savefig('data.png', dpi = 250, format = 'png', transparent = True, bbox_inches = 'tight')
-        else:
-            plt.plot(x, y, color = '#287086', ls = '-')
-            plt.title(eq)
-            plt.xlabel('$' + v_x + "(" + u_x + ")" + '$')
-            plt.ylabel('$' + v_y + "(" + u_y + ")" + '$')
-            plt.savefig('equation.png', dpi = 250, format = 'png', transparent = True, bbox_inches = 'tight')
+    if latex == True:
+        try:
+            rc('font',**{'family':font_family,font_family:[font]})
+            rc('text', usetex=True)
+            rcParams['text.latex.unicode'] = unicode
+            rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
+            rcParams['axes.labelsize'] = 15
+        except IndexError:
+            print("Make sure that latex is installed in your machine including the amsmath package or disable latex in the configuration.py file.")
+
+    if graph == 1:
+        plt.plot(x, y,
+                 ls = data_graph_line_type, color = data_graph_line_color, lw = data_graph_line_width,
+                 marker = data_graph_marker_type, mec = data_graph_marker_color, mew = data_graph_marker_edge_width, mfc = data_graph_marker_edge_color, ms = data_graph_marker_size,
+                 alpha = data_graph_transparency, aa = antialiased)
+        plt.xlabel('$' + v_x + "(" + u_x + ")" + '$')
+        plt.ylabel('$' + v_y + "(" + u_y + ")" + '$')
+        plt.savefig('data.' + save_format, dpi = save_dpi, frameon = save_frameon, format = save_format, transparent = save_transparent, bbox_inches = save_bbox_inches, orientation = save_orientation)
     else:
-        if graph == 1:
-            plt.plot(x, y, color = '#361156', ls = '-', marker = '.')
-            plt.xlabel('$' + v_x + "(" + u_x + ")" + '$')
-            plt.ylabel('$' + v_y + "(" + u_y + ")" + '$')
-            plt.savefig('data.png', dpi = 250, format = 'png', transparent = True, bbox_inches = 'tight')
-        else:
-            plt.plot(x, y, color = '#287086', ls = '-')
-            plt.title(eq)
-            plt.xlabel('$' + v_x + "(" + u_x + ")" + '$')
-            plt.ylabel('$' + v_y + "(" + u_y + ")" + '$')
-            plt.savefig('equation.png', dpi = 250, format = 'png', transparent = True, bbox_inches = 'tight')
+        plt.plot(x, y,
+                 ls = equation_graph_line_type, color = equation_graph_line_color, lw = equation_graph_line_width,
+                 alpha = equation_graph_transparency, aa = antialiased)
+        plt.title(eq)
+        plt.xlabel('$' + v_x + "(" + u_x + ")" + '$')
+        plt.ylabel('$' + v_y + "(" + u_y + ")" + '$')
+        plt.savefig('equation.' + save_format, dpi = save_dpi, frameon = save_frameon, format = save_format, transparent = save_transparent, bbox_inches = save_bbox_inches, orientation = save_orientation)
     plt.show()
 
     if graph != 1:
-        comp = input('Do you want to compare the equation with the entered data? (y/n)')
+        comp = input(C3 + 'Do you want to compare the equation with the entered data? (y/n) ' + CE)
         if comp == 'y':
-            plt.plot(x, y, color = '#287086', ls = '-')
-            plt.plot(load(open('x.dump', 'rb')), load(open('y.dump', 'rb')), color = '#361156', ls='None', marker = '.')
+            plt.plot(x, y,
+                     ls = comparison_graph_equation_line_type, color = comparison_graph_equation_line_color, lw = comparison_graph_equation_line_width,
+                     alpha = comparison_graph_equation_transparency, aa = antialiased)
+            plt.plot(load(open('x.dump', 'rb')), load(open('y.dump', 'rb')),
+                     ls = comparison_graph_data_line_type, color = comparison_graph_data_line_color, lw = comparison_graph_data_lide_width,
+                     marker = comparison_graph_data_marker_type, mec = comparison_graph_data_marker_color, mew = comparison_graph_data_marker_edge_width, mfc = comparison_graph_data_marker_edge_color, ms = comparison_graph_data_marker_size,
+                     alpha = comparison_graph_data_transparency, aa = antialiased)
 
-            patch_1 = mpatches.Patch(color='#361156', label= r'\textrm{\textnormal{Data}}')
-            patch_2 = mpatches.Patch(color='#287086', label= eq)
+            patch_1 = mpatches.Patch(color = comparison_graph_data_line_color, label= r'\textrm{\textnormal{Data}}')
+            patch_2 = mpatches.Patch(color = comparison_graph_equation_line_color, label= eq)
 
             plt.legend(handles=[patch_1, patch_2])
 
             plt.xlabel('$' + v_x + "(" + u_x + ")" + '$')
             plt.ylabel('$' + v_y + "(" + u_y + ")" + '$')
 
-            plt.savefig('comparison.png', dpi = 250, format = 'png', transparent = True, bbox_inches = 'tight')
+            plt.savefig('comparison.' + save_format, dpi = save_dpi, frameon = save_frameon, format = save_format, transparent = save_transparent, bbox_inches = save_bbox_inches, orientation = save_orientation)
             plt.show()
 
 def D(max_v, min_v):
@@ -80,7 +84,7 @@ def D(max_v, min_v):
 
 
 def csv_file(rows):
-    with open('data.csv', 'w', encomin_vng='utf8') as csvfile:
+    with open('data.csv', 'w', encoding = 'utf8') as csvfile:
         writer = csv.writer(csvfile)
         for row in rows:
             writer.writerow(row)
